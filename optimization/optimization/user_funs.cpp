cdacda06be16e79fa5b1ca94242874a038e54c48
Wojciech Szewczyk <154233420+vvszewczyk@users.noreply.github.com>
2024-10-17 12:15:33 +0200

    Add realistic problem func

--- optimization/optimization/user_funs.cpp
+++ optimization/optimization/user_funs.cpp
@@ -39,3 +39,27 @@ matrix df1(matrix x, matrix ud1, matrix ud2)
 	y = -cos(0.1 * x(0)) * exp(-pow(0.1 * x(0) - (2 * 3.14), 2)) + 0.002 * pow(0.1 * x(0), 2);
 	return y;
 }
+
+matrix f1(double t, matrix Y, matrix ud1, matrix ud2)
+{
+	const double a = 0.98;
+	const double b = 0.63;
+	const double g = 9.81;
+	const double PA = 0.75;
+	const double TA = 90;
+	const double PB = 1.0;
+	const double DB = 0.00365665;
+	const double Fin = 0.01;
+	const double Tin = 10;
+
+	matrix dY(3, 1);
+
+	const double FAout = (Y(0) > 0) ? (a * b * m2d(ud2) * sqrt(2 * g * Y(0) / PA)) : 0;
+	const double FBout = (Y(1) > 1) ? (a * b * DB * sqrt(2 * g * Y(1) / PB)) : 0;
+
+	dY(0) = -FAout;
+	dY(1) = FAout + Fin - FBout;
+	dY(2) = (Fin / Y(1)) * (Tin - Y(2)) + (FAout / Y(1)) * (TA - Y(2));
+
+	return dY;
+}
